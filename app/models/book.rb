b55c5a36fb7b8e81d0b5499045107d3726ae2848
charliestarkpl <k.michbiotech@gmail.com>
2018-03-18 20:14:44 +0100

    Add Scope for Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,10 @@
 class Book < ApplicationRecord
-  has_many :loans
   belongs_to :author
+  has_many :loans
+  has_many :users, through: :loans
   has_many :comments, as: :commentable, dependent: :destroy
 
-  scope :
+  scope :checkout_history, -> (book) { includes(:loans).where('loans.book_id = :id', id: book.id) }
+  scope :active_loans, -> { includes(:loans).where(loans: { status: 'active' }) }
   validates :title, presence: true
 end

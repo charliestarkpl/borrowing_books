b0c5ac339134f3438c570e28f8e5917d62780306
charliestarkpl <k.michbiotech@gmail.com>
2018-03-18 20:17:28 +0100

    Add resources to Routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
-  resources :book
+  resources :books
+  resources :authors
+  resources :comments
 end

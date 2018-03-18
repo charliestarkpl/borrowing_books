a89f9d8422bd7646e8d1f040132b2dcfc362050b
charliestarkpl <k.michbiotech@gmail.com>
2018-03-18 19:55:43 +0100

    Add relations between Models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,8 @@
 class Book < ApplicationRecord
+  has_many :loans
+  belongs_to :author
+  has_many :comments, as: :commentable, dependent: :destroy
+
+  scope :
+  validates :title, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,23 @@
 class User < ApplicationRecord
+  has_many :loans
+  has_many :books, through: :loans
+  has_many :comments
+
   devise :omniauthable, omniauth_providers: [:google_oauth2]
+
+  scope :with_books, -> { joins(:books, :loans) }
+
+  before_destroy :return_books
+
 
   def self.from_omniauth(auth)
     where(provider: auth[:provider], uid: auth[:uid]).first_or_create do |user|
       user.name = auth[:info][:name]
       user.email = auth[:info][:email]
     end
+  end
+
+  def return_books
+    loans.each { |b| b.update(status: 1) }
   end
 end

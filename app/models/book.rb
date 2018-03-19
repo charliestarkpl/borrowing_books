598fa135b7e76f7f12ea4a3807408def96c7f7ca
charliestarkpl <k.michbiotech@gmail.com>
2018-03-19 02:50:32 +0100

    Add scope to Book

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,10 +5,16 @@ class Book < ApplicationRecord
   has_many :comments, as: :commentable, dependent: :destroy
 
   scope :with_open_loans, -> { includes(:loans).where(loans: { status: 'active' }) }
+  scope :with_authors, -> { includes(:author) }
 
   validates :title, presence: true
 
   def toggle_borrow!
      update borrowed: !borrowed
+  end
+
+  def borrowed_by?(user)
+    loan = Loan.book_by_user(self, user).where(status: 'active')&.take
+    loan.nil?
   end
 end
